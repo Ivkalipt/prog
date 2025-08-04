650b9daf0575e232221d5f164c264b48db29dff5
Ivkalipt <kirill.sedov1010@gmail.com>
2025-08-04 19:04:53 +0500

    to commit

--- tbank/dp2/J.cpp
+++ tbank/dp2/J.cpp
@@ -1,44 +1,47 @@
-#pragma GCC optimize ("O3,unroll-loops")
-// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
-// #pragma GCC optimize ("Ofast")
-
+#pragma GCC optimize("Ofast,unroll-loops")
 #include <bits/stdc++.h>
-
-#define ll long long
-#define ld long double
-#define vll vector<ll>
-#define pll pair<ll, ll>
-#define vpll vector<pair<ll, ll>>
-#define vvll vector<vector<ll>>
-#define fastio cin.tie(0)->sync_with_stdio(0);
-#define F first
-#define S second
-#define all(a) a.begin(), a.end() 
-
 using namespace std;
 
-const ll inf = 1e16;
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define rep(i, a, b) for (ll i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)(x).size()
+#define F first
+#define S second
+#define pb push_back
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+constexpr ll INF = 9;
+
+ll rec(ll l, ll r, vvll & dp, string & s) {
+    // cout << '!';
+    if (dp[l][r] != INF) return dp[l][r];
+    if (l > r) return INF;
+    if (l == r) dp[l][r] = 0;
+    else if (r - l == 1) dp[l][r] = (s[l] == s[r] ? 0 : 1);
+    else dp[l][r] = rec(l + 1, r - 1, dp, s) + (s[l] == s[r] ? 0 : 1);
+    return dp[l][r];
+}
 
 int main() {
     ll n, k;
     cin >> n >> k;
     string s;
     cin >> s;
-    vvll dp(n, vll(n, 0));
-    for(ll i = 0; i < n; i++) {
-        dp[i][i] = 0;
-        dp[i][min(i + 1, n - 1)] = (s[i] != s[min(i + 1, n - 1)]);
-    }
-
-    for(ll d = 3; d <= n; d++) {
-        for(ll l = 0, r = l + d - 1; r < n; r++, l++) {
-            dp[l][r] = (s[l] != s[r]) + dp[l + 1][r - 1];
+    vector dp(n, vector (n, INF));
+    for (ll l = 0; l < n; l++) {
+        for (ll r = l; r < n; r++) {
+            dp[l][r] = rec(l, r, dp, s);
         }
     }
     ll cnt = 0;
-    for(ll i = 0; i < n; i++) {
-        for(ll j = i; j < n; j++) {
-            if(dp[i][j] <= k) cnt++;
+    rep(i, 0, n) {
+        rep(j, 0, n) {
+            if (dp[i][j] <= k) cnt++;
         }
     }
     cout << cnt << '\n';
